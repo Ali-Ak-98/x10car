6503bb5be1dada654b5997cba4a1e7e193889ed9
ali <ali1998akbarzadeh@gmail.com>
2021-04-26 12:16:44 +0430

    add test api

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,22 @@ import styles from '../styles/Home.module.css'
 import axios from "axios";
 
 export async function getServerSideProps(context) {
-    const url = 'https://autoextract.scrapinghub.com/v1/extract';
-    const data = {
-        'url': 'https://allegro.pl/uzytkownik/bula_lukasz/lampy-przednie-i-elementy-lampy-przednie-255099?bmatch=cl-e2101-d3794-c3683-aut-1-2-0412',
-        'pageType': 'productList',
-    };
-    const options = {
-        headers: {
-            'Authorization': 'Basic ' + Buffer.from('6db07fd5272a431483ff1ed3c530911a:').toString('base64'),
-            'Content-Type': 'application/json'
-        },
-    };
-
-    const res = await axios.post(url, data, options);
+    // const url = 'https://autoextract.scrapinghub.com/v1/extract';
+    // const data = {
+    //     'url': 'https://allegro.pl/uzytkownik/bula_lukasz/lampy-przednie-i-elementy-lampy-przednie-255099?bmatch=cl-e2101-d3794-c3683-aut-1-2-0412',
+    //     'pageType': 'productList',
+    // };
+    // const options = {
+    //     headers: {
+    //         'Authorization': 'Basic ' + Buffer.from('6db07fd5272a431483ff1ed3c530911a:').toString('base64'),
+    //         'Content-Type': 'application/json'
+    //     },
+    // };
+    //
+    // const res = await axios.post(url, data, options);
 
     return {
-        props: {data: res.data}, // will be passed to the page component as props
+        props: {data: ''}, // will be passed to the page component as props
     }
 }
 
