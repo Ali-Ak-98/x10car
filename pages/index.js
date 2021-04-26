4266eed3ce12339f6ccda7aca9630a71bb1b6b6a
ali <ali1998akbarzadeh@gmail.com>
2021-04-26 12:02:23 +0430

    add test api

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,60 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import axios from "axios";
 
 export async function getServerSideProps(context) {
-    const https = require('https');
-    let resData = {};
-
-    const data = JSON.stringify([{
+    const url = 'https://autoextract.scrapinghub.com/v1/extract';
+    const data = {
         'url': 'https://allegro.pl/uzytkownik/bula_lukasz/lampy-przednie-i-elementy-lampy-przednie-255099?bmatch=cl-e2101-d3794-c3683-aut-1-2-0412',
         'pageType': 'productList',
-    }]);
+    };
     const options = {
-        host: 'autoextract.scrapinghub.com',
-        path: '/v1/extract',
         headers: {
             'Authorization': 'Basic ' + Buffer.from('6db07fd5272a431483ff1ed3c530911a:').toString('base64'),
-            'Content-Type': 'application/json',
-            'Content-Length': data.length
+            'Content-Type': 'application/json'
         },
-        method: 'POST',
     };
-    const req = https.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`);
-        res.on('data', d => {
-            resData = d;
-            process.stdout.write(d);
-        });
-    });
-    req.on('error', error => {
-        console.error(error);
-    });
-    req.write(data);
-    await req.end();
+
+    const res = await axios.post(url, data, options);
+
     return {
-        props: {data: JSON.stringify(resData)}, // will be passed to the page component as props
+        props: {data: res.data}, // will be passed to the page component as props
     }
 }
 
 export default function Home(props) {
-    console.log(props);
+
+    const test = async () => {
+        const https = require('https');
+
+        const data = JSON.stringify([{
+            'url': 'https://allegro.pl/uzytkownik/bula_lukasz/lampy-przednie-i-elementy-lampy-przednie-255099?bmatch=cl-e2101-d3794-c3683-aut-1-2-0412',
+            'pageType': 'productList',
+        }]);
+        const options = {
+            host: 'autoextract.scrapinghub.com',
+            path: '/v1/extract',
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from('6db07fd5272a431483ff1ed3c530911a:').toString('base64'),
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            },
+            method: 'POST',
+        };
+        const req = https.request(options, res => {
+            console.log(`statusCode: ${res.statusCode}`);
+            res.on('data', d => {
+                process.stdout.write(d);
+                console.log(d);
+            });
+        });
+        req.on('error', error => {
+            console.error(error);
+        });
+        req.write(data);
+        req.end();
+
+    }
 
     return (
         <div className={styles.container}>
@@ -47,7 +64,7 @@ export default function Home(props) {
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>
+                <h1 className={styles.title} onClick={test}>
                     Welcome to <a href="https://nextjs.org">Next.js!</a>
                 </h1>
 
