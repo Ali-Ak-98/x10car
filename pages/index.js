9744a9e0a2298abdd11a3f70786ecbb7a066794e
ali <ali1998akbarzadeh@gmail.com>
2021-04-26 13:17:50 +0430

    add test api

--- pages/index.js
+++ pages/index.js
@@ -28,10 +28,10 @@ export default function Home(props) {
 
     const test = async () => {
         const url = 'https://autoextract.scrapinghub.com/v1/extract'
-        const data = {
+        const data = [{
             'url': 'https://allegro.pl/uzytkownik/bula_lukasz/lampy-przednie-i-elementy-lampy-przednie-255099?bmatch=cl-e2101-d3794-c3683-aut-1-2-0412',
             'pageType': 'productList',
-        };
+        }];
         const options = {
             headers: {
                 'Authorization': 'Basic ' + Buffer.from('6db07fd5272a431483ff1ed3c530911a:').toString('base64'),
